ecc379bfe9bbeffc3b0d785a89a2b6e10f2c3c1d
hyunganom <dlguswo0504@naver.com>
2022-05-05 14:25:24 +0900

    dddddd

---
+++ day 09/src/day09/For2.java
@@ -0,0 +1,19 @@
+package day09;
+
+public class For2 {
+
+	public static void main(String[] args) {
+		int sum=0;
+		for(int i=1; i<=10; i++) {
+		System.out.printf("i=%d, sum=%d\n",i,sum+=i);
+		}
+		
+		//int sum=0;
+		//for(int i=1; i<=10 ; i++) {
+		//	sum = sum+i;//1,3,6
+		//}
+		//System.out.println(sum);
+		
+	}
+
+}
---
+++ day 09/src/day09/Practice.java
@@ -0,0 +1,20 @@
+package day09;
+
+public class Practice {
+
+	public static void main(String[] args) {
+		/*
+		for(int i=1;i<=10;i++) {
+			System.out.println(i+"번 컴퓨터 켜줘" );
+		}
+		*/
+		for(int i=1;i<=10;i++) {
+			if(i%3==0) {
+				System.out.println(i+"번 컴퓨터 켜줘");
+			}
+		}
+		
+		
+	}
+
+}

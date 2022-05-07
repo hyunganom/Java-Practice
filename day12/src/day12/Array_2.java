10ea365f31b1b8ba5c727008aeafe9a00e1db726
hyunganom <dlguswo0504@naver.com>
2022-05-07 22:17:50 +0900

    dddd

--- day12/src/day12/Array_2.java
+++ day12/src/day12/Array_2.java
@@ -8,9 +8,10 @@ public class Array_2 {
 		int[]a = new int[]   {10,20,4,25,18};
 		//b라는 정수형 배열을 생성할때, 초기값으로 넣는 방법 new int[]생략가능
 		int[]b = {1,2,3,4,5,6,7};
-		
+		//c라는 정수형 배열을 선언하고, 10개의 정수들을 묶을 수 있는 크기 할당
 		int c[]=new int[10];
 		
+		//length<<이거는 배열의 전체길이
 		for(int i=0;i<c.length;i++) {
 			c[i]=i;
 		}

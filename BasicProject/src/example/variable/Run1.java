fde92e2f6f47e910048c2e6099450239a2fc2f77
hyunganom <dlguswo0504@naver.com>
2022-05-22 22:00:43 +0900

    복습1

---
+++ BasicProject/src/example/variable/Run1.java
@@ -0,0 +1,12 @@
+package example.variable;
+
+public class Run1 {
+
+	public static void main(String[] args) {
+		Variable va1 = new Variable();
+		//va1.initVariable();
+		
+		va1.changeValue();
+	}
+
+}

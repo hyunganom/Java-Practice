84cf7cdac1925b796b044187106fb59cb3d68b39
hyunganom <dlguswo0504@naver.com>
2022-06-03 20:41:59 +0900

    클래스와 객체생성

---
+++ JAVA/src/OOP/Class_1.java
@@ -0,0 +1,7 @@
+package OOP;
+
+public class Class_1 {}//소스파일의 이름은 public class이름과 일치!!!
+// public class hello{}: 오류 하나의 소스파일에는 하나의 public class만 허용
+class hello1{}
+//하나의 소스파일에는 하나의 클래스만 작성하는 것이 바람직
+

3797d5ee5f4375e265e783144171083b1f5294e2
hyunganom <dlguswo0504@naver.com>
2022-06-04 01:34:10 +0900

    객체배열 == 참조변수 배열

---
+++ JAVA/src/OOP/Array_Oop.java
@@ -0,0 +1,15 @@
+package OOP;
+
+public class Array_Oop {
+
+}
+//	객체배열 == 참조변수 배열
+//	Tv tv1, tv2, tv3; ----> Tv[] tvArr = new Tv[3];	
+
+//	Tv tvArr = new Tv[3]; --->길이가 3인 tv타입의 참조변수 배열
+//	객체를 생성해서 배열의 각 요소에 저장
+//	tvArr[0] = new Tv();
+//	tvArr[1] = new Tv();
+//	tvArr[2] = new Tv();
+	
+//	주의사항 : 객체를 저렇게 만들었을뿐 안에 값이 안담겨있다. 조심하자(객체의 주소만 만들었음)

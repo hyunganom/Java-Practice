d97457b921e5cba01e293babd42f12bec69471b6
hyunganom <dlguswo0504@naver.com>
2022-06-09 19:27:22 +0900

    기본형 매개변수

---
+++ JAVA/src/OOP/Parameter1.java
@@ -0,0 +1,20 @@
+package OOP;
+
+public class Parameter1 {
+	public static void main(String[] args) {
+	Date d = new Date();
+	d.x =10;
+	System.out.println("main() : x = "+d.x);
+	change(d.x);
+	System.out.println("After change(d.x)");
+	System.out.println("main() : x = "+d.x);
+	
+	}
+	static void change(int x) {//기본형 매개변수
+		x = 1000;
+		System.out.println("change() : x =" + x);
+	}
+}
+class Date{int x;}
+//기본형 매개변수 - 변수의 값을 읽기만 할 수 있다.(read only)
+//참조형 매개변수 - 변수의 값을 읽고 변경할 수 있다.

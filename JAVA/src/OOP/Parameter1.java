5550658adbf7c07ae053e7fb5d4924272b6522c7
hyunganom <dlguswo0504@naver.com>
2022-06-11 21:11:19 +0900

    기본형 매개변수

--- JAVA/src/OOP/Parameter1.java
+++ JAVA/src/OOP/Parameter1.java
@@ -11,10 +11,12 @@ public class Parameter1 {
 	
 	}
 	static void change(int x) {//기본형 매개변수
-		x = 1000;
+		x = 1000; // 여기서 d.x의 값이 변경된게 아니라 매개변수 x가 변경된거임 (지역변수)
 		System.out.println("change() : x = " + x);
 	}
 }
 class Date{int x;}
 //기본형 매개변수 - 변수의 값을 읽기만 할 수 있다.(read only)
 //참조형 매개변수 - 변수의 값을 읽고 변경할 수 있다.
+
+//매개변수 : 지역변수의 일종으로 메소드 호출할때 값을 넣어줌

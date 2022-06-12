9d81f8e770509dfc61f97162c7abfa5dbcf80a29
hyunganom <dlguswo0504@naver.com>
2022-06-12 18:54:03 +0900

    생성자

--- JAVA/src/OOP/Constructor2.java
+++ JAVA/src/OOP/Constructor2.java
@@ -10,7 +10,7 @@ public class Constructor2 {
 }
 class Date_1{
 	int value;
-	Date_1(){} //기본생성자 추가
+//	Date_1(){} //기본생성자 추가 이게 없어도됨 왜냐면 생성자가 1개라도 있으면 이걸 만들어줘야하는데 지금 안에 생성자가 없으니 자동으로 만들어줌
 }
 
 class Date_2{
@@ -20,5 +20,5 @@ class Date_2{
 		value=x;
 	}
 
-	Date_2() {}// 기본 생성자
+	Date_2() {}// 기본 생성자 위에 하나가 있으니 기본생성자를 만들어 줘야함
 }
